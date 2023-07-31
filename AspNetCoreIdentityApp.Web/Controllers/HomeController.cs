b3ecbe163ad38ca0f049b10f2335068709f01be4
Yahya KARAYEL <karayelyahya@gmail.com>
2023-07-31 21:10:54 +0300

    Add project files.

---
+++ AspNetCoreIdentityApp.Web/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using AspNetCoreIdentityApp.Web.Models;
+using AspNetCoreIdentityApp.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AspNetCoreIdentityApp.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly UserManager<AppUser> _userManager;
+		public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager)
+		{
+			_logger = logger;
+			_userManager = userManager;
+		}
+
+		public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+		public async Task<IActionResult> SignUp(SignUpViewModel request)
+		{
+            var identityResult =await _userManager.CreateAsync(new() { UserName = request.UserName, PhoneNumber = request.Phone, Email = request.Email }, request.PasswordConfirm);
+            if (identityResult.Succeeded)
+            {
+                TempData["SuccessMessage"] = "Üyelik kayıt işlemi başarıyla gerçekleştirilmiştir.";
+                return RedirectToAction(nameof(HomeController.SignUp));
+            }
+
+            foreach(IdentityError item in identityResult.Errors)
+            {
+                ModelState.AddModelError(string.Empty, item.Description);
+            }
+
+            return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
